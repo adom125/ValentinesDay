2b8e72f67dcfb3dfcc15b7c493e2a2f5e43f22ba
Alexandra Domanski <150095101+adom125@users.noreply.github.com>
2025-02-05 22:43:37 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function startFloatingHearts() 
+{
+    const heartContainer = document.getElementById('heart-container');
+    const numberOfHearts = 11;
+
+    for (let i = 0; i < numberOfHearts; i++) {
+        createHeart();
+    }
+
+    function createHeart()
+     {
+        const heart = document.createElement('div');
+        heart.classList.add('heart');
+        heart.innerHTML = '♥︎';  
+        
+        const randomX = Math.random() * 100 - 50;  
+        const randomY = Math.random() * -500 - 200;  
+
+        heart.style.setProperty('--rand-x', `${randomX}%`);
+        heart.style.setProperty('--rand-y', `${randomY}px`);
+
+        heart.style.left = `${Math.random() * 100}%`; 
+        heart.style.animationDelay = `${Math.random() * 1}s`;  
+
+        heartContainer.appendChild(heart);
+
+        setTimeout(() => {
+            heart.remove();
+        }, 5000);  
+    }
+}
